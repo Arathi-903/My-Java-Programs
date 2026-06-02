ee617a65285f59967a6ef542cf729e3f09744cf7
Arathi-903 <aarathijenne583@gmail.com>
2026-06-02 11:21:48 +0530

    Add files via upload

---
+++ Autoboxing_Autounboxing.java
@@ -0,0 +1,25 @@
+package Langugefundamentals_datatypes;
+
+public class Autoboxing_Autounboxing {
+	//Autoboxing;
+	int b  = 100;
+	Integer c = b;
+			
+	//Auto_Unboxing;
+	Integer d = 100;
+	int e = d;
+
+	public static void main(String[] args) {
+		Autoboxing_Autounboxing a1 = new Autoboxing_Autounboxing();
+		
+		System.out.println(a1.b);
+		System.out.println(a1.c);
+		System.out.println(a1.d);
+		System.out.println(a1.e);
+		
+		
+		
+
+	}
+
+}
